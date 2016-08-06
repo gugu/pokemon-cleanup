6728e9f0c89e36371fc0e75b57b1240eb17e9af6
Andrii Kostenko <andrey@kostenko.name>
2016-08-06 19:52:17 +0300

    Add UI

--- pokemgr.py
+++ pokemgr.py
@@ -1,30 +1,20 @@
 import pgoapi
 import json
 import time
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return """<form method="post" action="/cleanup">
-        <div><label>Username <input type="text" name="username" /></label></div>
-        <div><label>Password <input type="password" name="password" /></label></div>
-        <div><label>Account type <select name="type">
-            <option value="google">Google</option>
-            <option value="ptc">Pokemon go</option>
-        </select></label></div>
-        <div><label>Leave <input name="n" type="number" value="2" /> pokemon each type</label></div>
-        <div><label>Leave pokemons with CP > <input name="cp" type="number" value="500" /></label></div>
-        <input type="submit" />
-        <a href="https://github.com/gugu/pokemon-cleanup">source code</a>
-    </form>
-    """
+    return render_template('index.html')
 
 @app.route('/cleanup', methods=['POST'])
 def cleanup():
     api = pgoapi.PGoApi()
     api.set_position(40.7127837, -74.005941, 0.0)
-    assert api.login(request.form['type'], request.form['username'], request.form['password'])
+    auth_success = api.login(request.form['type'], request.form['username'], request.form['password'])
+    if not auth_success:
+        return "Auth failure"
     inventory_items = api.get_inventory()['responses']['GET_INVENTORY']['inventory_delta']['inventory_items']
     pokemons = [ii['inventory_item_data']['pokemon_data'] for ii in inventory_items if 'pokemon_data' in ii['inventory_item_data']]
     pokemon_by_type = {}
